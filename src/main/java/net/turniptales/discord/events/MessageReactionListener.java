1ae9f1d19f6000b07ce9c3f977054f905c0d75d0
rettichlp <rettichlp01@gmail.com>
2024-02-12 00:01:23 +0100

    Fix votes not working for multi users

--- src/main/java/net/turniptales/discord/events/MessageReactionListener.java
+++ src/main/java/net/turniptales/discord/events/MessageReactionListener.java
@@ -2,6 +2,7 @@ package net.turniptales.discord.events;
 
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.channel.unions.MessageChannelUnion;
+import net.dv8tion.jda.api.entities.emoji.EmojiUnion;
 import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -15,8 +16,12 @@ public class MessageReactionListener extends ListenerAdapter {
         MessageChannelUnion channel = e.getChannel();
         User user = e.getUser();
         if (channel.equals(SURVEY_TEXT_CHANNEL) && nonNull(user) && !user.isBot()) {
-            channel.retrieveMessageById(e.getMessageIdLong()).queue(message -> message.getReactions()
-                    .forEach(messageReaction -> messageReaction.removeReaction(user).queue()));
+            channel.retrieveMessageById(e.getMessageIdLong()).queue(message -> message.getReactions().forEach(messageReaction -> messageReaction.retrieveUsers().queue(users -> {
+                EmojiUnion emoji = messageReaction.getEmoji();
+                if (users.contains(user) && !emoji.equals(e.getEmoji())) {
+                    message.removeReaction(emoji, user).queue();
+                }
+            })));
         }
     }
 }
